975c99632d752f9b9fd2db83d4aed258121715b8
DanielMarella <marella.daniel96@gmail.com>
2023-06-06 17:09:41 +0200

    add function for remove element from the list

--- js/myscript.js
+++ js/myscript.js
@@ -21,6 +21,15 @@ const { createApp } = Vue
 
         addToDoElment (newElement) {
             this.toDoList.push(newElement);
+            this.newElement = '';
+        },
+
+        removeElement (toDoElementIndex) {
+            if(toDoElementIndex >= this.toDoList.length || toDoElementIndex < 0){
+                alert("you can't do it");
+            } else{
+                this.toDoList.splice(toDoElementIndex, 1);
+            }
         }
 }
 }).mount('#app');
@@ -33,14 +42,14 @@ const { createApp } = Vue
 
 //     Descrizione:
 // Rifare l'esercizio della to do list. Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
-// text, una stringa che indica il testo del todo
+// text, una stringa che indica il testo del todo X
 // done, un booleano (true/false) che indica se il todo è stato fatto oppure no
 
 // MILESTONE 1
-// Stampare all'interno di una lista HTML un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// Stampare all'interno di una lista HTML un item per ogni todo. X Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato. 
 
 // MILESTONE 2
-// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// Visualizzare a fianco ad ogni item ha una "x" X: cliccando su di essa, il todo viene rimosso dalla lista.
 
 // MILESTONE 3
-// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti. X
